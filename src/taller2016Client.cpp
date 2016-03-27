255b6895ba482d282c7b235ff0bb509efb23534f
peragu <agustinperrotta_31@@hotmail.com>
2016-03-26 21:08:49 -0300

    cliente genera un mensaje y se lo envia al server cada vez q lo corro.

--- src/taller2016Client.cpp
+++ src/taller2016Client.cpp
@@ -1,15 +1,81 @@
-//============================================================================
-// Name        : taller2016Client.cpp
-// Author      : Taller2016
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <strings.h>
+#include <stdio.h>
+#include <sys/unistd.h>
+#include <string.h>
+#define MAXHOSTNAME 256
 using namespace std;
 
+struct clientMsj {
+	int id;
+	char type[20];
+	char value[20];
+};
+
+int initializeClient(string destinationIp, int port) {
+	struct sockaddr_in remoteSocketInfo;
+	struct hostent *hPtr;
+	int socketHandle;
+	const char *remoteHost = destinationIp.c_str();
+	int portNumber = port;
+
+	bzero(&remoteSocketInfo, sizeof(sockaddr_in));  // Clear structure memory
+
+	// Get system information
+
+	if ((hPtr = gethostbyname(remoteHost)) == NULL) {
+		cerr << "System DNS name resolution not configured properly." << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	// create socket
+
+	if ((socketHandle = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		close(socketHandle);
+		exit(EXIT_FAILURE);
+	}
+
+	// Load system information into socket data structures
+
+	memcpy((char *) &remoteSocketInfo.sin_addr, hPtr->h_addr, hPtr->h_length);
+	remoteSocketInfo.sin_family = AF_INET;
+	remoteSocketInfo.sin_port = htons((u_short) portNumber);  // Set port number
+
+	if (connect(socketHandle, (struct sockaddr *) &remoteSocketInfo,
+			sizeof(sockaddr_in)) < 0) {
+		close(socketHandle);
+		exit(EXIT_FAILURE);
+	}
+
+	return socketHandle;
+
+}
+
+int write_socket(int destinationSocket, void *buf, int len) {
+	int currentsize = 0;
+	int count = 0;
+	while (currentsize < len) {
+		count = write(destinationSocket, buf + currentsize, 1);
+		if (count < 0)
+			return -1;
+		currentsize += 1;
+	}
+	return currentsize;
+}
+
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+
+	//genero un mensaje de prueba para enviar al server
+	clientMsj testMsj = { };
+	testMsj.id = 10;
+	strcpy(testMsj.type, "mensaje de prueba");
+	strcpy(testMsj.value, "avanzar nave");
+
+	int destinationSocket = initializeClient("127.0.0.1", 8080);
+	write_socket(destinationSocket, &testMsj, 44);
+
 }
