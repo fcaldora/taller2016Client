f71d7b49efa470ec10ee7c93bc5834715decdf0e
luciano <lucianomintrone@gmail.com>
2016-03-29 17:07:29 -0300

    lm-Create one thread for printing server messages and other one for reading server messages

--- src/taller2016Client.cpp
+++ src/taller2016Client.cpp
@@ -9,10 +9,33 @@
 #include <string.h>
 #include "XmlParser.h"
 #include "CargadorXML.h"
+#include <list>
 
 #define MAXHOSTNAME 256
 using namespace std;
 
+list<clientMsj> messagesList;
+pthread_t clientThreadID[3];
+
+struct thread_args {
+    int socketConnection;
+}args;
+
+int readBlock(int fd, void* buffer, int len) {
+	int ret = 0;
+	int count = 0;
+	while (count < len) {
+		ret = read(fd, buffer + count, 1);
+		if (ret = 0) {
+			return 0;
+		}
+		if (ret < 0){
+			return -1;
+		}
+		count += 1;
+	}
+	return count;
+}
 
 int initializeClient(string destinationIp, int port) {
 	struct sockaddr_in remoteSocketInfo;
@@ -50,7 +73,6 @@ int initializeClient(string destinationIp, int port) {
 	}
 
 	return socketHandle;
-
 }
 
 int write_socket(int destinationSocket, void *buf, int len) {
@@ -65,6 +87,50 @@ int write_socket(int destinationSocket, void *buf, int len) {
 	return currentsize;
 }
 
+void* readServerMessages(void* parameter){
+	int socketConnection = args.socketConnection;
+	while(1) {
+		clientMsj msj = { };
+		int result = readBlock(socketConnection, &msj, 60);
+		if (result = 0){
+			// error or eof
+			cout << "EOF";
+		}
+		if (result < 0){
+			cout << "ERROR";
+			// error
+		}
+	}
+}
+
+void* readMsjs(void *param) {
+	clientMsj clientMessage = { };
+	while (1) {
+		if (!messagesList.empty()) {
+			clientMessage = messagesList.front();
+			cout << clientMessage.id << endl;
+			cout << clientMessage.value << endl;
+			cout << clientMessage.type << endl;
+			messagesList.pop_front();
+		}
+	}
+	return 0;
+}
+
+void initThreadForReadingFromSocket(int socketConnection) {
+	thread_args args;
+	args.socketConnection = socketConnection;
+	int error = pthread_create(&(clientThreadID[3]), NULL, &readServerMessages, &args);
+	if (error != 0)
+			printf("\ncan't create thread :[%s]", strerror(error));
+}
+
+void initThreadForReadingMessages(){
+	int error = pthread_create(&(clientThreadID[2]), NULL, &readMsjs, NULL);
+	if (error != 0)
+		printf("\ncan't create thread :[%s]", strerror(error));
+}
+
 int main(int argc, char* argv[]) {
 	if(argc!=2){
 		cout<<"Falta escribir el nombre del archvo!"<<endl;
@@ -77,14 +143,27 @@ int main(int argc, char* argv[]) {
 	strcpy(testMsj.type, "mensaje de prueba");
 	strcpy(testMsj.value, "avanzar nave");
 	CargadorXML cargador;
+
+	//cargador.cargarServidor("clienteTest.txt");
 	cargador.cargarServidor(argv[1]);
+
 	XmlParser parser(cargador.getDocumento());
 	string ip;
 	int puerto;
 	parser.obtenerIp(ip);
-	parser.obtenerPuertoCl(puerto);	
+	parser.obtenerPuertoCl(puerto);
 
 	int destinationSocket = initializeClient(ip, puerto);
+	initThreadForReadingFromSocket(destinationSocket);
+	initThreadForReadingMessages();
 	write_socket(destinationSocket, &testMsj, 60);
 
+	messagesList.push_back(testMsj);
+
+	pthread_join(clientThreadID[2], NULL);
+	pthread_join(clientThreadID[3], NULL);
+	close(destinationSocket);
+
+	cout << "EXIT_SUCCESS";
+	return EXIT_SUCCESS;
 }
